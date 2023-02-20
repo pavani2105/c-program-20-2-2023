8dde1102f896bae6e701ffd074a4d58beabbf03b
pavani2105 <125651883+pavani2105@users.noreply.github.com>
2023-02-20 19:15:14 +0530

    Add files via upload

---
+++ open5.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	 int a[100],i,j,n,s;
+	 scanf("%d",&n);
+	 for(i=0;i<n;i++)
+	 {
+	 	scanf("%d",&a[i]);
+	 }
+	 for(i=0;i<n;i++)
+	 {
+	 	for(j=i+1;j<n;j++)
+	 	{
+	 		if(a[i]<a[j])
+	 		{
+	 		 s=a[i];
+			 a[i]=a[j];
+			 a[j]=s;
+		 }
+	 }}
+	 printf("%d",a[1]);
+}
